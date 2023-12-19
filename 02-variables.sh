3001694f7a419c04cb8d8ffbe984d54e813400e6
Giridhar <giri.ge@mymail.com>
2023-12-19 13:40:42 +0530

    you updated

--- 02-variables.sh
+++ 02-variables.sh
@@ -6,5 +6,5 @@ PERSON2=monika
 echo "$PERSON1: Hello $PERSON2, good morning"
 echo "$PERSON2: Hello $PERSON1, very good morning"
 echo "$PERSON1: How r youuuuuu $PERSON2"
-echo "$PERSON2: im good $PERSON1, how r u?"
+echo "$PERSON2: im good $PERSON1, how r youu?"
 
