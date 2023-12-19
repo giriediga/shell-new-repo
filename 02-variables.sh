bd81b0d4665af91a2c5ca5bc9e60810fa3188210
Giridhar <giri.ge@mymail.com>
2023-12-19 17:35:22 +0530

    you updated

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-PERSON1=$3
+PERSON1=$1
 PERSON2=$7
 
-echo "$PERSON3: Hello $PERSON7, good morning"
-echo "$PERSON7: Hello $PERSON3, very good morning"
-echo "$PERSON3: How r youuuuuu $PERSON7"
-echo "$PERSON7: im good $PERSON3, how r youu?"
+echo "$PERSON1: Hello $PERSON7, good morning"
+echo "$PERSON7: Hello $PERSON1, very good morning"
+echo "$PERSON1: How r youuuuuu $PERSON7"
+echo "$PERSON7: im good $PERSON1, how r youu?"
 
