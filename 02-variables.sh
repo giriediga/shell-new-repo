e143df430d5821d769780c5cd40c5b5ec1455078
Giridhar <giri.ge@mymail.com>
2023-12-19 12:54:29 +0530

    this is variable example

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "Ramesh: Hello Suresh, good morning"
-echo "Suresh: Hello Ramesh, very good morning"
-echo "Ramesh: How r youuuuuu"
-echo "Suresh: im good, how r u?"
+PERSON1:Ramesh
+PERSON2:Suresh
+
+echo "$PERSON1: Hello $PERSON2, good morning"
+echo "$PERSON2: Hello $PERSON1, very good morning"
+echo "$PERSON1: How r youuuuuu $PERSON2"
+echo "$PERSON2: im good $PERSON1, how r u?"
+
